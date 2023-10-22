bd2fd4fa1e2ab25d8e0919d01d3e1d994bbfead2
Mariana Pineda Miranda <111026594+mpinedam@users.noreply.github.com>
2023-10-22 20:17:34 +0000

    Cambios estilo

--- TallerTypeScript1/main.ts
+++ TallerTypeScript1/main.ts
@@ -7,10 +7,11 @@ let seriesTable: HTMLElement = document.getElementById("serie")!;
 mostrarDatosSerie(listaseries);
 
 function mostrarDatosSerie(allseries: series[]): void {
-  let tbodyAprendiz = document.createElement("tbody");
   
   allseries.forEach((serie) => {
-    tbodyAprendiz.innerHTML = `<tr><td>${serie.id}</td> <td>${serie.name}</td> <td>${serie.channel}</td> <td>${serie.seasons}</td></tr>`
+    let tbodyAprendiz = document.createElement("tr");
+    tbodyAprendiz.innerHTML = `<tr><td>${serie.id}</td> <td>${serie.name}</td> <td>${serie.channel}</td> <td>${serie.seasons}</td> </tr>`;
+    seriesTable.appendChild(tbodyAprendiz);
   }) ;
-  seriesTable.appendChild(tbodyAprendiz);
+
 }
--- TallerTypeScript1/scripts/main.js
+++ TallerTypeScript1/scripts/main.js
@@ -2,9 +2,9 @@ import { listaseries } from "./data.js";
 var seriesTable = document.getElementById("serie");
 mostrarDatosSerie(listaseries);
 function mostrarDatosSerie(allseries) {
-    var tbodyAprendiz = document.createElement("tbody");
     allseries.forEach(function (serie) {
-        tbodyAprendiz.innerHTML = "<tr><td>".concat(serie.id, "</td> <td>").concat(serie.name, "</td> <td>").concat(serie.channel, "</td> <td>").concat(serie.seasons, "</td></tr>");
+        var tbodyAprendiz = document.createElement("tr");
+        tbodyAprendiz.innerHTML = "<tr><td>".concat(serie.id, "</td> <td>").concat(serie.name, "</td> <td>").concat(serie.channel, "</td> <td>").concat(serie.seasons, "</td> </tr>");
+        seriesTable.appendChild(tbodyAprendiz);
     });
-    seriesTable.appendChild(tbodyAprendiz);
 }
