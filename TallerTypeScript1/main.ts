6645b70a5924435f944234fb74597d2e4f732285
Mariana Pineda Miranda <111026594+mpinedam@users.noreply.github.com>
2023-10-22 19:27:24 +0000

    Commit con series

--- TallerTypeScript1/main.ts
+++ TallerTypeScript1/main.ts
@@ -1,29 +1,24 @@
 import {series} from "./series.js"
 
-export const serie1 = new series(1, "Modern Family", "BBC", 10)
+export const serie1 = new series(1, "Modern Family", "Star+", 10)
+export const serie2 = new series(1, "Stranger Things", "Netflix", 4)
+export const serie3 = new series(1, "Gossip Girl", "HBO", 10)
+export const serie4 = new series(1, "Gilmore Girls", "Netflix", 10)
+export const serie5 = new series(1, "Ginny and Georgia", "Netflix", 10)
+
 
 let seriesTable: HTMLElement = document.getElementById("serie")!;
 
-mostrarDatosSerie(serie1);
+mostrarDatosSerie(serie1, serie2, serie3, serie4, serie5);
 
-function mostrarDatosSerie(serie1: series):void{
+function mostrarDatosSerie(serie1: series, serie2: series, serie3:series, serie4:series, serie5:series):void{
     let tbodyAprendiz = document.createElement("tbody");
-    tbodyAprendiz.innerHTML = ` <tr>
-    <th scope="col">#</th>
-    <th scope="col">Id</th>
-    <th scope="col">Nombre</th>
-    <th scope="col">Channel</th>
-    <th scope="col">Seasons</th>
-  </tr>
-
-  <tr>
-    <th scope="row">1</th>
-    <td>${serie1.id}</td>
-    <td>${serie1.name}</td>
-    <td>${serie1.channel}</td>
-    <td>${serie1.seasons}</td>
-  </tr>
+    tbodyAprendiz.innerHTML = ` <tr><td>Id</td> <td>Nombre</td> <td>Channel</td> <td>Seasons</td></tr>
+    <tr><td>${serie1.id}</td> <td>${serie1.name}</td> <td>${serie1.channel}</td> <td>${serie1.seasons}</td></tr>
+    <tr><td>${serie2.id}</td> <td>${serie2.name}</td> <td>${serie2.channel}</td> <td>${serie2.seasons}</td></tr>
+    <tr><td>${serie3.id}</td> <td>${serie3.name}</td> <td>${serie3.channel}</td> <td>${serie3.seasons}</td></tr>
+    <tr><td>${serie4.id}</td> <td>${serie4.name}</td> <td>${serie4.channel}</td> <td>${serie4.seasons}</td></tr>
+    <tr><td>${serie5.id}</td> <td>${serie5.name}</td> <td>${serie5.channel}</td> <td>${serie5.seasons}</td></tr>
     `
     seriesTable.appendChild(tbodyAprendiz);
 }
-
